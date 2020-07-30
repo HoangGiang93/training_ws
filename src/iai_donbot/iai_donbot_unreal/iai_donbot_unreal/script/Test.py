da696bd85b51dc65c4732ced3fe73a76234cd362
Hoang Giang Nguyen <hoanggia@uni-bremen.de>
2020-07-30 12:41:01 +0200

    Fix Camera Rotation

--- src/iai_donbot/iai_donbot_unreal/iai_donbot_unreal/script/Test.py
+++ src/iai_donbot/iai_donbot_unreal/iai_donbot_unreal/script/Test.py
@@ -20,7 +20,7 @@ r_goal = PoseStamped()
 r_goal.header.frame_id = 'gripper_tool_frame'
 r_goal.header.stamp = rospy.get_rostime()
 r_goal.pose.position = Point(0.41, -0.535, 0.0)
-r_goal.pose.orientation = Quaternion(0.0, 0.0, math.sqrt(2)/2, math.sqrt(2)/2)
+r_goal.pose.orientation = Quaternion(0.0, 0.0, -math.sqrt(2)/2, math.sqrt(2)/2)
 giskard_wrapper.set_cart_goal('base_footprint', 'gripper_tool_frame', r_goal)
  
 giskard_wrapper.plan_and_execute()
